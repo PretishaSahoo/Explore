259e352eb3b635a5719ee43fd66f91d71542da98
Pretisha Sahoo <115354635+PretishaSahoo@users.noreply.github.com>
2023-10-08 17:26:22 +0530

    Add files via upload

---
+++ src/Components/NewsItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function NewsItem(props){
+    let {title,description,imageUrl,url,author,date}=props;
+    return (
+      <div>
+        <div className="card my-3" >
+            <img src={imageUrl} className="card-img-top" style={{ height: "17rem" }} alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}....</h5>
+                <p className="card-text">{description}...</p>
+                <p className="card-text"><small className="text-body-secondary">By {author} on {new Date(date).toGMTString()}</small></p>
+                <a href={url} className="btn btn-dark" target="blank">Read More</a>
+            </div>
+            </div>
+      </div>
+    )
+}
+
+
